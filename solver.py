22fc509072410ef8d0222169de239e925fe36d15
Lauren Braun <braun@pdx.edu>
2018-07-03 10:28:04 -0700

    update to reflect argument changes in load maze func

--- solver.py
+++ solver.py
@@ -1,10 +1,12 @@
 # L.Braun 2018
 # Main program to solve a gridworld maze problem
 
+# Uses qlearner.py, environ.py
+
 from qlearner import QLearner
 
 a_learner = QLearner()
-a_learner.load_maze('/u/braun/tlab/QLearner/data/reward_4x4.npy', '/u/braun/tlab/QLearner/data/next_4x4.npy', '/u/braun/tlab/QLearner/data/meta_4x4.txt')
+a_learner.load_maze('/u/braun/tlab/QLearner/data/reward_4x4.npy', '/u/braun/tlab/QLearner/data/meta_4x4.txt')
 
 print ("testing data load\n\n")
 
