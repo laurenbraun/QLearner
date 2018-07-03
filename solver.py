b581a039ae62b06064a8d811dc2977318b5651d2
Lauren Braun <braun@pdx.edu>
2018-07-03 15:16:48 -0700

    updated driver program to reflect class changes

--- solver.py
+++ solver.py
@@ -4,14 +4,15 @@
 # Uses qlearner.py, environ.py
 
 from qlearner import QLearner
+import pylab as plt
 
 my_learner = QLearner()
 my_learner.load_maze('/u/braun/tlab/QLearner/data/reward_4x4.npy', '/u/braun/tlab/QLearner/data/meta_4x4.txt')
 
 #print ("testing data load\n\n")
 
-my_learner.display_Q()
-my_learner.display_R()
+#my_learner.display_Q()
+#my_learner.display_R()
 
 print ("begin training...")
 
@@ -20,6 +21,12 @@ reward = my_learner.train(0.7)
 my_learner.display_Q()
 my_learner.display_R()
 
+steps = my_learner.test()
+print "steps"
+print steps
+print ""
 
+#plt.plot(reward)
+#plt.show()
 
 
